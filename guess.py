0a35450edbe7f1d9fc7ac286240ade782e832528
Gus <gumarques@gmail.com>
2019-08-20 22:04:44 +0100

    Improving Guess the number Game

--- guess.py
+++ guess.py
@@ -1,14 +1,37 @@
+# -*- coding: UTF-8 -*-
+
 print("*********************************")
-print("Guess a number games!")
+print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
-secret = 8
+numero_secreto = 5
+tentativas = 3
+rodada = 1
 
-your_guess = input("Type a number from 0 to 10: ")
+while(rodada <= tentativas):
+    print('Tentativa ', rodada, ' de ', tentativas, '.')
+    chute = int(input("Digite um número entre 1 e 10: "))
+    print("Você digitou " , chute)
+    if chute not in range(1,10):
+        print("Número incorreto!!!", "\n")
+        continue
 
-print('Your guess: ', your_guess)
+#    chute_str = int(input("Digite um número entre 0 e 10: "))
+#    print("Você digitou " , chute_str)
+#    chute = int(chute_str)
 
-if(secret == int(your_guess)):
-    print('Very good!')
-else:
-    print('Oops, not this time!')
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if(acertou):
+        print("Parabéns! Você acertou!", "\n")
+        exit() #sai do jogo se acertar
+    elif(maior):
+            print("O seu chute foi maior do que o número secreto!", "\n")
+    elif(menor):
+            print("O seu chute foi menor do que o número secreto!", "\n")
+    rodada += 1
+
+
+print("Fim do jogo")
