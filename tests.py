72dbb8e9b87989752ba3ad85f31f3a975f084c7e
Gus <gumarques@gmail.com>
2019-08-20 16:19:05 +0100

    syntax sugar

--- tests.py
+++ tests.py
@@ -5,3 +5,12 @@ print('Brasil', 'ganhou', 5, 'Mundiais', sep='-', end='\n')
 print('Brasil', 'ganhou', 5, 'Mundiais', end='')
 
 print('Brasil', 'ganhou', 5, 'Mundiais', sep='-', end='!\n')
+
+# Python nao faz conversao automatica.
+# Multiplicar 10 x "20" ele faz algo chamado syntax sugar
+# Q eh imprimir 10x o texto "20"
+
+numero1 = 10
+numero2 = "20"
+produto = numero1 * numero2
+print(produto)
