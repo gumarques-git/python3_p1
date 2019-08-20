31b7d700591faa1a2687c7a8a066ba83ce97704e
Gus <gumarques@gmail.com>
2019-08-20 16:56:13 +0100

    ELIF melhorado

--- elif.py
+++ elif.py
@@ -9,13 +9,13 @@ guess = input("Type a number from 0 to 10: ")
 your_guess = int(guess)
 
 acertou = your_guess == secret
+maior = your_guess > secret
+menor = your_guess < secret
 
 if (acertou):
     print("Você acertou!")
 else:
-    if (your_guess > secret):
+    if (maior):
         print("Você errou! O seu chute foi maior que o número secreto.")
-    elif (your_guess < 3):
-        print("Você erro.")
-    elif (your_guess < secret):
+    elif (menor):
         print("Você errou! O seu chute foi menor que o número secreto.")
