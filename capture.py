5d11f643591dece87c3e4922dad13ef52135af8e
Gus <gumarques@gmail.com>
2019-08-20 16:11:29 +0100

    updates

--- capture.py
+++ capture.py
@@ -1,8 +1,4 @@
-print("*********************************")
-print("Bem vindo ao jogo de Adivinhação!")
-print("*********************************")
 
-numero_secreto = 42
 
 chute = input("Digite o seu número: ")
 
