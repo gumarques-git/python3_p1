3fc473b8919b524c8ac9eb95fe90a45fa408547e
Gus <gumarques@gmail.com>
2019-08-20 23:04:17 +0100

    Using For and Range

--- guess.py
+++ guess.py
@@ -5,14 +5,13 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = 5
-tentativas = 3
-rodada = 1
+tentativas = 4
 
-while(rodada <= tentativas):
+for rodada in range(1,tentativas + 1):
     print('Tentativa {} de {}.'.format(rodada, tentativas)) #String Interpolation
     chute = int(input("Digite um número entre 1 e 10: "))
     print("Você digitou " , chute)
-    if chute not in range(1,10):
+    if (chute < 1 or chute >10):
         print("Número incorreto!!!", "\n")
         continue # pula o codigo abaixo e volta ao inicio do loop
 
@@ -22,12 +21,10 @@ while(rodada <= tentativas):
 
     if(acertou):
         print("Parabéns! Você acertou!", "\n")
-        exit() # sai do loop se acertar
+        break # sai do loop se acertar
     elif(maior):
             print("O seu chute foi maior do que o número secreto!", "\n")
     elif(menor):
             print("O seu chute foi menor do que o número secreto!", "\n")
-    rodada += 1
-
 
 print("Fim do jogo")
